c08421d832300953623465f86ea34fe898e37b94
Xavier Uldry <xavier.uldry@gmx.ca>
2024-10-21 16:39:45 -0700

    Simple File backup
    A bash script used on Windows

--- README.md
+++ README.md
@@ -14,6 +14,20 @@
 
 	Exports all of them for backup purpose. (for automated backups, the path needs to be taken care of)
 	
+### SimpleFileBackup
+	
+	The story is that I just wanted to backup a small binary file from one cloud drive to another cloud. Boths drives are transcient.
+	I'm already using a backup solution, but it couldn't do something so basic.
+	I was in a bash mood, so I used git bash and set a windows task to trigger every hour.
+		
+	#### archive_file.sh <path-filename>
+	It backs up the file if it has changed, A bit to bit comparison had to be used due to metadata being part of hashes, so if the file big, rely on the modification date.
+	A timestamp is added to the file name of the older version (the creation date is prepended)
+	A couple of things are logged, errors and each time a new version is archived.  
+	The past 5 versions or the past 5 days (Up to you) are kept That's it!
+	
+	Scheduler command: C:\Program Files\Git\git-bash.exe"  --hide "C:\Users\<USER>\bin\archive_file.sh" <FILE NAME>	
+
 ### trstat.py
 
 	Small utility to get the file statistics for the tree on a given path
@@ -21,3 +35,4 @@
 	Gives the number of files, folders, maximum tree depth and also details the size stats for each file extension, 
 	number of files, total size occupied, min, max 	and average file size.
 	It can be useful to find files that shouldn't be in this path or that have an abnormal size.
+	
---
+++ SimpleFileBackup/archive_file.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# This scripts archives the 5 last versions of a file in a different location
+
+set -e
+
+# Set log file
+log_file="C:\\Users\\<NAME>\\<FOLDER>\\logfile.log"
+
+# Set source and destination directories
+source_dir="Z:\\<FOLDER>\\"
+dest_dir="//<FOLDER>"
+
+# Get the file name
+file_name=$(basename "$1")
+
+# Check if source and destination directories exist
+if [ ! -d "$source_dir" ] || [ ! -d "$dest_dir" ]; then
+  echo "$(date) - Error: Source or destination directory is not available." >> "$log_file"
+  echo "Error: Source or destination directory is not available." 
+  exit 1
+fi
+
+# Check if file exists in source directory
+if [ ! -f "$source_dir/$file_name" ]; then
+  echo "$(date) - Error: Source file '$file_name' does not exist or partition not mounted." >> "$log_file"
+  echo "$Error: File '$file_name' does not exist in source directory '$source_dir'." 
+  exit 1
+fi
+
+# Check if file exists in destination directory
+if [ -f "$dest_dir/$file_name" ]; then
+  # Compare files
+  if cmp -s "$source_dir/$file_name" "$dest_dir/$file_name"; then
+    #echo "$(date) - Skipping copy: Source and destination files are identical." >> "$log_file"
+	echo "Skipping copy: Source and destination files are identical." 
+    exit 0
+  else
+    # Archive previous version
+    timestamp=$(date -r "$dest_dir/$file_name" "+%Y-%m-%d-%H%M%S")
+    echo "$(date) - Archiving previous version with the following date: '${timestamp}'" >> "$log_file"
+	echo "Archiving previous version with the following date: '${timestamp}'"
+    if ! mv "$dest_dir/$file_name" "$dest_dir/${timestamp}_${file_name}"; then
+	 echo "$(date) - Could not rename current file" >> "$log_file"
+	 echo "Could not rename current file"
+	 exit 1
+	fi
+  fi
+fi
+
+# Copy the new version to the destination
+if cp "$source_dir/$file_name" "$dest_dir/${file_name}"; then
+ echo "$(date) - Copied '$file_name' to destination directory." >> "$log_file"
+ echo "Copied '$file_name' to destination directory."
+else
+ echo "$(date) - Could not copy to destination" >> "$log_file"
+ echo " Could not copy to destination"
+ exit 1
+fi 
+
+# Keep only the past 5 days
+#find "$dest_dir" -name "*_$file_name" -type f -print0 | sort -z -r -t '_' -k 2 | tail -z -n +6 | xargs -0 -r rm
+
+# Keep only the last 5 version
+find "$dest_dir" -name "*_$file_name" -type f -print0 | sort -z -r -t '_' -k 2 | awk -vORS=$'\0' 'NR>5' | xargs -0 -r rm
+#echo "$(date) - Removed old versions of '$file_name' if any." >> "$log_file"
+echo "Removed old versions of '$file_name'." 
